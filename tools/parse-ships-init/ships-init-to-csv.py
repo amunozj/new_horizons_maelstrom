9bbc9896c50e1fb9166b9226b0cc9c8f8920f357
amunozj <amunozj@boulder.swri.edu>
2021-11-12 13:00:40 -0700

    create and save csv with models

--- tools/parse-ships-init/ships-init-to-csv.py
+++ tools/parse-ships-init/ships-init-to-csv.py
@@ -14,111 +14,7 @@ parser.add_argument(
     help='Location of the New Horizons mod'
 )
 
-class ship_model:
-    """
-    Model class to store a dictionary with all the necessary parameters as well
-        as flags to indicate gender and processing status
-    """
-
-    def __init__(self, model_list,
-                nh_dir=None):
-        """
-        Initialization function of the storm_model class
-
-        Parameters
-        ----------
-        model_list: list
-            list with all entries read from initModel.c
-        nh_dir: str
-            Location of the New Horizons mod
-        output_dir: str
-            Folder for converted output                   
-        """
-
-        self.nh_dir = nh_dir
-        self.output_dir = output_dir
-
-        # create list with animation as comment
-        self.comments_and_ending = ['//'+line for line in model_list if '.ani' in line and '//' not in line]
-        #add all comments and ending
-        self.comments_and_ending += [line for line in model_list if 'model.' not in line or '//' in line.split('model.')[0] or 'AssignModel' in line or 'IF' in line]
-
-        self.only_parameters = [line for line in model_list if 'model.' in line and '//' not in line.split('model.')[0] and 'AssignModel' not in line and '.ani' not in line]
-        self.model_parameters = {}
-
-        # Fill parameter dictionary
-        for line in self.only_parameters:
-            parameter = line.split('model.')[1].split('=')[0].replace(' ', '').replace('\t', '')
-            self.model_parameters[parameter] = line.split('=')[1].replace('\t', '').strip()
-        
-        # Make sure the model has an assigned sex
-        if 'sex' not in self.model_parameters:
-            self.model_parameters['sex'] = '"man";'
-
-        # Store flag to indicate that the model has an animation different than "man" or "woman_sit"
-        self.standard_ani = True
-        if 'ani' in self.model_parameters:
-            if self.model_parameters['ani'] == '"woman_sit";' or self.model_parameters['ani'] == '"man";':
-                self.standard_ani = True
-            else:
-                self.standard_ani = False
-
-        # Assemble model file name
-        self.model = self.model_parameters['id'].split('"')[1]
-        self.model_file = self.model + ".gm"
-
-        # clean sex string, set animation files, and conversion flags
-        self.convert_potc_to_coas_man=False
-        self.convert_potc_to_coas_woman=False
-        if 'woman' in self.model_parameters['sex']:
-            self.sex = 'woman'
-            self.an_path = self.output_dir + f'/RESOURCE/animation/woman_coas.an'
-            self.convert_potc_to_coas_woman = True
-            self.model_parameters['ani'] = '"woman_coas";'
-            
-        elif 'monkey' in self.model_parameters['sex']:
-            self.sex = 'monkey'
-
-        elif 'skeleton' in self.model_parameters['sex']:
-            self.sex = 'skeleton'
-            self.an_path = self.output_dir + f'/RESOURCE/animation/man_coas.an'
-            self.convert_potc_to_coas_man = True
-            self.model_parameters['ani'] = f'"man";'
-        else:
-            self.sex = 'man'
-            self.an_path = self.output_dir + f'/RESOURCE/animation/man_coas.an'
-            self.convert_potc_to_coas_man = True
-
-        # Define parameters for import
-        self.filepath = self.nh_dir + f'/RESOURCE/MODELS/Characters/{self.model_file}'
-        self.textures_path = self.nh_dir + '/RESOURCE/Textures/Characters/'
-        
-        self.fix_coas_man_head = False
-        self.convert_coas_to_potc_man = False
-        self.convert_coas_to_potc_woman = False
-
-        # Define parameters for export
-        self.filepath_out = self.output_dir + f'/RESOURCE/MODELS/Characters/{self.model_file}'
-
-
-    def print_to_file(self, initShipsOutputFile):
-        """
-        Function to print model into file
-
-        Parameters
-        ----------
-        initShipsOutputFile: file
-            file where to print the lines of each model       
-        """
-
-        for key, value in self.model_parameters.items():
-            initShipsOutputFile.write(f'\tmodel.{key} = {value}\n')
-
-        for line in self.comments_and_ending:
-            initShipsOutputFile.write(line)
-
-
-def ships_add_wake(
+def ships_make_csv(
     nh_dir=None
     ):
     """Batch process initModels.c
@@ -162,7 +58,8 @@ def ships_add_wake(
 
  
     initShipsFile.close()
-    
+    shipsdDf.to_csv('ship_init.csv')
+
  
 
 if __name__ == "__main__":
@@ -173,4 +70,4 @@ if __name__ == "__main__":
     args = vars(parser.parse_known_args(sys.argv)[0])
     print(args)
 
-    ships_add_wake(**args)
+    ships_make_csv(**args)
