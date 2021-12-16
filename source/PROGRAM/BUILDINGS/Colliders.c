9ce1f01363528367d96c0246a32745a022dfcd7b
amunozj <amunozj@boulder.swri.edu>
2021-12-16 00:19:36 -0500

    remove line from colliders

--- source/PROGRAM/BUILDINGS/Colliders.c
+++ source/PROGRAM/BUILDINGS/Colliders.c
@@ -180,4 +180,4 @@ float GetTriangleArea(float x0, float y0, float x1, float y1, float x2, float y2
 	 return area;
 }
 
-string sCurFog = Whr_GetCurrentFog();
+// string sCurFog = Whr_GetCurrentFog();
